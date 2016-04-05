c8bc03a3e5e11ac96d59bd6061b7b6eb0bb3f658
Moshe Nahmias <moshegrey@gmail.com>
2016-04-05 06:25:39 +0300

    Added my schedule tab and functionality.
    * Now when one sign the attend it goes to the my schedule tab and
      show there.
    * If one sign the attend while he/she is going to another event at
      the same time, a pop up will let one choose if to change selection
      or not.

--- www/js/data.js
+++ www/js/data.js
@@ -1,37 +1,33 @@
 var data = reader({
-    "time": 20160305172900,
+    "time": 20160313172902,
     "schedule": {
         "day1": {
             "track1": [
                 {
                     "title": "title 1",
                     "speaker": "speaker 1",
-                    "time": "time",
-                    "attend": "true",
+                    "time": "09:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 },
                 {
                     "title": "title 2",
                     "speaker": "speaker 2",
-                    "time": "time",
-                    "attend": "true",
+                    "time": "10:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 },
                 {
                     "title": "title 3",
                     "speaker": "speaker 3",
-                    "time": "time",
-                    "attend": "false",
+                    "time": "11:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 },
                 {
                     "title": "title 4",
                     "speaker": "speaker 4",
-                    "time": "time",
-                    "attend": "false",
+                    "time": "12:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 }
@@ -40,32 +36,28 @@ var data = reader({
                 {
                     "title": "title 5",
                     "speaker": "speaker 5",
-                    "time": "time",
-                    "attend": "false",
+                    "time": "09:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 },
                 {
                     "title": "title 6",
                     "speaker": "speaker 6",
-                    "time": "time",
-                    "attend": "false",
+                    "time": "10:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 },
                 {
                     "title": "title 7",
                     "speaker": "speaker 7",
-                    "time": "time",
-                    "attend": "true",
+                    "time": "11:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 },
                 {
                     "title": "title 8",
                     "speaker": "speaker 8",
-                    "time": "time",
-                    "attend": "true",
+                    "time": "12:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 }
@@ -76,32 +68,28 @@ var data = reader({
                 {
                     "title": "title 1",
                     "speaker": "speaker 9",
-                    "time": "time",
-                    "attend": "true",
+                    "time": "09:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 },
                 {
                     "title": "title 2",
                     "speaker": "speaker 10",
-                    "time": "time",
-                    "attend": "false",
+                    "time": "10:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 },
                 {
                     "title": "title 3",
                     "speaker": "speaker 11",
-                    "time": "time",
-                    "attend": "true",
+                    "time": "11:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 },
                 {
                     "title": "title 4",
                     "speaker": "speaker 12",
-                    "time": "time",
-                    "attend": "false",
+                    "time": "12:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 }
@@ -110,32 +98,28 @@ var data = reader({
                 {
                     "title": "title 5",
                     "speaker": "speaker 13",
-                    "time": "time",
-                    "attend": "false",
+                    "time": "09:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 },
                 {
                     "title": "title 6",
                     "speaker": "speaker 14",
-                    "time": "time",
-                    "attend": "true",
+                    "time": "10:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 },
                 {
                     "title": "title 7",
                     "speaker": "speaker 15",
-                    "time": "time",
-                    "attend": "false",
+                    "time": "11:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 },
                 {
                     "title": "title 8",
                     "speaker": "speaker 16",
-                    "time": "time",
-                    "attend": "true",
+                    "time": "12:00",
                     "speaker-description": "description of the speaker",
                     "lecture-abstruct": "abstract of the lecture"
                 }
--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,13 @@
 var app = {
     // Application Constructor
     initialize: function() {
-        this.populateData(data);
-        this.buildSchedule(data.schedule);
+        if (window.localStorage.length < 1 || app.getData('datetime') < data.time) {
+            this.populateData(data);
+        }
+        if (!app.getObjectData('mySchedule')) {
+            this.populateMySchedule();
+        }
+        this.buildSchedule(app.getObjectData('schedule'));
     },
 
     // Bind Event Listeners
@@ -14,19 +19,49 @@ var app = {
     },
 
     populateData: function (data) {
-        if (window.localStorage.length < 1) {
-            app.storeData('datetime', data.time);
-            for (var day in data.schedule) {
-                for (var track in data[day]) {
-                    for (i = 0; i < data[day][track].length; i++) {
-                        app.storeData(day + track + i + 'attend', data[day][track][i]['attend']);
-                    }
+        app.storeData('datetime', data.time);
+        app.storeObjectData("schedule", data.schedule);
+    },
+
+    populateMySchedule: function () {
+        var schedule = app.getObjectData('schedule');
+        var mySchedule = {};
+
+        for (var day in schedule) {
+            mySchedule[day] = {};
+            // assuming the tracks on a day are sincronized, if not should
+            // refactor to accomodate this.
+            for (var i = 0; i < schedule[day][Object.keys(schedule[day])[0]].length; i++) {
+                mySchedule[day][schedule[day][Object.keys(schedule[day])[0]][i]["time"]] = "";
+            }
+        }
+        this.storeObjectData("mySchedule", mySchedule);
+    },
+
+    buildMySchedule: function () {
+        var schedule = app.getObjectData('schedule');
+        var mySchedule = app.getObjectData('mySchedule');
+        var elemSchedule = document.getElementById('myschedule');
+
+        for (var day in mySchedule) {
+            for (var time in mySchedule[day]) {
+                if (mySchedule[day][time]) {
+                    var arr = mySchedule[day][time].split("-");
+                    var day = arr[0];
+                    var track = arr[1];
+                    var num = arr[2];
+
+                    var item = app.createElementWithAttribute('div', 'class', 'list-item');
+
+                    item.appendChild(app.buildItemText(schedule[day][track][num], day, track, num));
+                    elemSchedule.appendChild(item);
                 }
             }
         }
     },
 
-    populateMySchedule: function (data) {
+    showMySchedule: function () {
+        app.buildMySchedule();
     },
 
     storeObjectData: function (key, val) {
@@ -47,16 +82,59 @@ var app = {
     },
 
     lectureAttend: function() {
-        var id = this.id;
-        if (this.getAttribute('data-attend') == "true") {
-            this.setAttribute('class', 'item-attend');
-            this.setAttribute('data-attend', false);
-            app.storeData(id, false);
+        var modal = document.getElementById('modal');
+
+        app.storeData('conflicting-schedule', this.getAttribute('id'));
+        var arr = this.getAttribute('id').split('-');
+        var day = arr[0];
+        var track = arr[1];
+        var num = arr[2];
+
+        var schedule = app.getObjectData('schedule');
+        var mySchedule = app.getObjectData('mySchedule');
+
+        var time = schedule[day][track][num]['time'];
+        if (mySchedule[day][time]) {
+            modal.style.display = "block";
         } else {
-            this.setAttribute('class', 'item-attend attend');
-            this.setAttribute('data-attend', true);
-            app.storeData(id, true);
+            app.replaceEvent();
+            return;
         }
+
+        var txtTitle = document.createTextNode(schedule[day][track][num]['title']);
+
+        var replacedEvent = document.getElementById('replaced-event');
+        replacedEvent.appendChild(txtTitle);
+    },
+
+    cancelModal: function () {
+        var modal = document.getElementById('modal');
+        modal.style.display = 'none';
+    },
+
+    replaceEvent: function () {
+        var id = app.getData('conflicting-schedule');
+        var arr = id.split('-');
+        var day = arr[0];
+        var track = arr[1];
+        var num = arr[2];
+
+        var mySchedule = app.getObjectData('mySchedule');
+        var schedule = app.getObjectData('schedule');
+        var time = schedule[day][track][num].time;
+
+        if (mySchedule[day][time] == id) {
+            document.getElementById(id).setAttribute('class', 'item-attend');
+            mySchedule[day][time] = "";
+            app.storeObjectData('mySchedule', mySchedule);
+        } else {
+            document.getElementById(id).setAttribute('class', 'item-attend attend');
+            mySchedule[day][time] = id;
+            app.storeObjectData('mySchedule', mySchedule);
+        }
+
+        var modal = document.getElementById('modal');
+        modal.style.display = 'none';
     },
 
     showHide: function() {
@@ -74,17 +152,29 @@ var app = {
         var cls = target.getAttribute("class");
         target.setAttribute("class", cls + " active");
 
-        var el = document.getElementById(target.id);
-        var tracks = document.getElementsByClassName('track');
+        var days = document.getElementsByClassName('days');
 
-        Array.prototype.forEach.call(tracks, function(elem) {
-            elem.parentElement.setAttribute("class", "days");
-            if (elem.id != "list" + el.id) {
-                elem.setAttribute("class", "track hide");
-            } else {
-                elem.setAttribute("class", "track");
+        Array.prototype.forEach.call(days, function(elem) {
+            while (elem.firstChild) {
+                elem.removeChild(elem.firstChild);
             }
         });
+
+        if (target.id != 'myscheduletab') {
+            var myschedule = document.getElementById('myschedule');
+            while (myschedule.firstChild) {
+                myschedule.removeChild(myschedule.firstChild);
+            }
+            var schedule = app.getObjectData('schedule');
+            var arr = target.id.split('-');
+            var day = arr[0];
+            var track = arr[1];
+
+            var e = document.getElementById(day);
+            var track = app.buildTracks(schedule[day][track], day, track);
+            e.appendChild(track);
+        }
+
         var lectureText = document.getElementById('lecture-text');
         lectureText.setAttribute("class", "hide");
     },
@@ -111,16 +201,25 @@ var app = {
     },
 
     buildSchedule: function (data) {
+        var el = document.getElementById("track-tabs");
+        var mySchedule = app.createElementWithAttribute('div', 'id', 'myscheduletab');
+        app.bindEvent(mySchedule, app.showMySchedule);
+
+        mySchedule.setAttribute('class', 'tabs');
+        this.bindEvent(mySchedule, this.showHide);
+        var txtSchedule = document.createTextNode('My schedule');
+        mySchedule.appendChild(txtSchedule);
+        el.appendChild(mySchedule);
+
         for (var day in data) {
             for (var key in data[day]) {
                 var tabView = document.createElement("div");
-                tabView.setAttribute('id', day + key);
+                tabView.setAttribute('id', day + '-' + key);
                 tabView.setAttribute('class', 'tabs')
                 this.bindEvent(tabView, this.showHide);
 
                 var txtDiv = document.createTextNode(day + " " + key);
                 tabView.appendChild(txtDiv);
-                var el = document.getElementById("track-tabs");
                 el.appendChild(tabView);
             }
         }
@@ -129,10 +228,6 @@ var app = {
             var dayTrack = document.createElement("div");
             dayTrack.setAttribute("class", "days");
             dayTrack.setAttribute("id", day);
-            for (var key in data[day]) {
-                var track = this.buildTracks(data[day][key], day, key);
-                dayTrack.appendChild(track);
-            }
             var body = document.getElementById('body');
             body.appendChild(dayTrack);
         }
@@ -140,41 +235,45 @@ var app = {
 
     buildTracks: function (items, day, track) {
         var trackDiv = document.createElement('div');
-        trackDiv.setAttribute('id', 'list' + day + track);
+        trackDiv.setAttribute('id', 'list' + '-' + day + '-' + track);
         trackDiv.setAttribute('class', 'track');
+
+        var mySchedule = this.getObjectData('mySchedule');
 
         for (var i = 0; i < items.length; i++) {
             var item = document.createElement('div');
             item.setAttribute('class', "list-item");
+
             item.appendChild(this.buildItemText(items[i], day, track, i));
-            item.appendChild(this.buildItemAttend(items[i], day, track, i));
+            item.appendChild(this.buildItemAttend(mySchedule[day][items[i].time], day, track, i));
             trackDiv.appendChild(item);
         }
 
         return trackDiv;
     },
 
+    createElementWithAttribute: function (tag, attr, name) {
+        var elem = document.createElement(tag);
+        elem.setAttribute(attr, name);
+        return elem;
+    },
+
     buildItemText: function (item, day, track, num) {
-        var txt = document.createElement('div');
-        txt.setAttribute('class', "item-text");
-        txt.setAttribute('id', day + "-" + track + "-" + num);
+        var txt = this.createElementWithAttribute('div', 'class', 'item-text');
 
         this.bindEvent(txt, this.lectureDetails);
 
-        var title = document.createElement('p');
-        title.setAttribute('class', 'item-title');
+        var title = this.createElementWithAttribute('p', 'class', 'item-title');
 
         var txtTitle = document.createTextNode(item.title);
         title.appendChild(txtTitle);
 
-        var speaker = document.createElement('p');
-        speaker.setAttribute('class', 'item-speaker');
+        var speaker = this.createElementWithAttribute('p', 'class', 'item-speaker');
 
         var spk = document.createTextNode(item.speaker);
         speaker.appendChild(spk);
 
-        var time = document.createElement('p');
-        time.setAttribute('class', 'item-time');
+        var time = this.createElementWithAttribute('p', 'class', 'item-time');
 
         var t = document.createTextNode(item.time);
         time.appendChild(t);
@@ -188,11 +287,16 @@ var app = {
     buildItemAttend: function (item, day, track, num) {
         var div = document.createElement('div');
         div.setAttribute('class', "item-attend");
-        div.setAttribute('id', day + track + num + "attend");
-        div.setAttribute('data-attend', item.attend);
+        div.setAttribute('id', day + "-" + track + "-" + num);
         this.bindEvent(div, this.lectureAttend);
 
-        if (app.getData(day + track + num + 'attend') == "true") {
+        var btnOK = document.getElementById('btn-ok');
+        var btnCancel = document.getElementById('btn-cancel');
+
+        app.bindEvent(btnOK, app.replaceEvent);
+        app.bindEvent(btnCancel, app.cancelModal);
+
+        if (item == day + "-" + track + "-" + num) {
             div.setAttribute('class', 'item-attend attend');
         } else {
             div.setAttribute("class", 'item-attend');
